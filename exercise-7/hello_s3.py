d9e4f5fffb6811f6fd67e2cf648c5ae6630b51f0
John Petersen <petersen.john@gmail.com>
2021-07-11 00:42:45 -0700

    Getting ready for exercise

--- exercise-7/hello_s3.py
+++ exercise-7/hello_s3.py
@@ -1,7 +1,10 @@
 import boto3
 
+# From https://boto3.amazonaws.com/v1/documentation/api/latest/guide/quickstart.html
+
 # Let's use Amazon S3
 s3 = boto3.resource("s3")
 
+# Print out bucket names
 for bucket in s3.buckets.all():
     print(bucket.name)
